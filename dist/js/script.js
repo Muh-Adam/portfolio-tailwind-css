f9b58cc19262dbb9cc76adfc340c9543264c9b78
Muh-Adam <adamyasin258@gmail.com>
2024-09-25 20:16:05 +0800

    update dark mode + perbaikan fitur

--- dist/js/script.js
+++ dist/js/script.js
@@ -18,3 +18,28 @@ hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
 });
+
+//DarkMode Toggle
+const darkToggle = document.querySelector("#dark-toggle");
+const html = document.querySelector("html");
+
+darkToggle.addEventListener("click", function () {
+  if (darkToggle.checked) {
+    html.classList.add("dark");
+    localStorage.theme = "dark";
+  } else {
+    html.classList.remove("dark");
+    localStorage.theme = "light";
+  }
+});
+
+//Pindahkan Posisi Toggle sesuai Mode
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkToggle.checked = true;
+} else {
+  darkToggle.checked = false;
+}
